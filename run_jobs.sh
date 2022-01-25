c010d89192a5442497ad8aaf08097364821412b0
Pavel Dmytrenko <pavdmyt@aiven.io>
2022-01-25 16:45:49 +0200

    Pass credentials explicitly; add gitignore

--- run_jobs.sh
+++ run_jobs.sh
@@ -4,20 +4,23 @@
 ##
 # python es_test_data.py --es_url=http://localhost:9200 --count=100 --index_name=fo-bar --num_of_shards=5 --num_of_replicas=1
 
-ES_HOST="localhost"
-ES_PORT="9200"
+SERVICE_URI=""
+USER=""
+PWD=""
 
 DOC_COUNT="1000"
-NUM_SHARDS=2
+NUM_SHARDS=1
 NUM_REPLICAS=1
 
-for i in $(seq 0 30); do
-  date_str=$(gdate -d "2019-07-01 +$i days" +"%Y%m%d")
-  idx_name="fluentd-foo-${date_str}"
+for i in $(seq 0 1); do
+  date_str=$(date -d "2022-01-23 +$i days" +"%Y%m%d")
+  idx_name="foo-${date_str}"
   echo ";;; Writing to index: $idx_name"
 
-  python es_test_data.py \
-    --es_url=http://${ES_HOST}:${ES_PORT} \
+  python ./elasticsearch-test-data/es_test_data.py \
+    --es_url=${SERVICE_URI} \
+    --username=${USER} \
+    --password=${PWD} \
     --count=${DOC_COUNT} \
     --index_name=${idx_name} \
     --num_of_shards=${NUM_SHARDS} \
